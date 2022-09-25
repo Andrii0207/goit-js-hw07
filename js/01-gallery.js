0127db649d1dd55305b18ebc0061e429ed31c8a3
Andrii Popov <popov.andrey80@gmail.com>
2022-09-25 23:27:11 +0300

    task-01 almost done!

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,29 +1,67 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const galleryEl = document.querySelector('.gallery');
 
-function galleryMarkup() {
+function galleryMarkup(galleryItems) {
   return galleryItems
     .map(
-      ({ preview, original, description }) =>
+      item =>
         `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
+  <a class="gallery__link" href="${item.original}">
     <img
       class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"/>
   </a>
 </div>`,
     )
     .join('');
 }
 
-const createGalleryItem = galleryMarkup(galleryItems);
-console.log(createGalleryItem);
+// function galleryMarkup(galleryItems) {
+//   return galleryItems.reduce(
+//     (acc, { original, preview, description }) =>
+//       acc +
+//       `<div class="gallery__item">
+//   <a class="gallery__link" href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       data-source="${original}"
+//       alt="${description}"
+//     />
+//   </a>
+// </div>`,
+//     '',
+//   );
+// }
 
-galleryEl.insertAdjacentHTML('beforeend', createGalleryItem);
+const createGalleryItem = galleryMarkup(galleryItems);
+galleryEl.innerHTML = createGalleryItem;
+
+galleryEl.addEventListener('click', onOpenModal);
+
+function onOpenModal(evt) {
+  evt.preventDefault();
+
+  //   if (!evt.target.classList.contains('gallery__image')) {
+  //     return console.log("It's not a picture");
+  //   }
+  const originalSizeImg = evt.target.dataset.source;
+
+  const instance = basicLightbox.create(`<img src="${originalSizeImg}"/>`);
+  instance.show();
+
+  window.addEventListener('keydown', onCloseModal);
+
+  function onCloseModal(evt) {
+    if (evt.code !== 'Escape') {
+      return console.log("It's not Escape key");
+    }
+    instance.close();
+  }
+}
