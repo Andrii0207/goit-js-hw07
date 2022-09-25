c97ed1f6833fcf9efd73cea55bcbdb4904c59227
Andrii Popov <popov.andrey80@gmail.com>
2022-09-25 20:30:56 +0300

    create galleryItem

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+function galleryMarkup() {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`,
+    )
+    .join('');
+}
+
+const createGalleryItem = galleryMarkup(galleryItems);
+console.log(createGalleryItem);
+
+galleryEl.insertAdjacentHTML('beforeend', createGalleryItem);
